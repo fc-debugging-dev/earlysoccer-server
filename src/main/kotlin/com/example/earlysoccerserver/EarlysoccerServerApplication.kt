dc159112cf0fae544effce509687a4b77431c294
KangJuSeong <wntjd3686@gmail.com>
2023-05-10 22:32:21 +0900

    chore. project init(jdk17, java17, springboot3.0.7)

---
+++ src/main/kotlin/com/example/earlysoccerserver/EarlysoccerServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.earlysoccerserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EarlysoccerServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EarlysoccerServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/earlysoccerserver/EarlysoccerServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.earlysoccerserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EarlysoccerServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
