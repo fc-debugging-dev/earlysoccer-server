20c46a5f9ed88acd605a0f62bf0261ae972ba10a
KangJuSeong <wntjd3686@gmail.com>
2023-05-12 14:14:30 +0900

    feat. member entity 작성

--- src/main/kotlin/com/example/earlysoccerserver/domain/member/Member.kt
+++ src/main/kotlin/com/example/earlysoccerserver/domain/member/Member.kt
@@ -1,4 +1,32 @@
 package com.example.earlysoccerserver.domain.member
 
-class Member {
+import com.example.earlysoccerserver.domain.AuditDateTimeEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import jakarta.validation.constraints.NotBlank
+
+@Entity
+@Table(name = "member")
+class Member(
+    email: String,
+    password: String,
+    name: String,
+    nickname: String,
+    profileImg: String
+): AuditDateTimeEntity() {
+    @Column(name = "email") @NotBlank
+    val email: String = email
+
+    @Column(name = "password") @NotBlank
+    val password: String = password
+
+    @Column(name = "name") @NotBlank
+    val name: String = name
+
+    @Column(name = "nickname") @NotBlank
+    val nickname: String = nickname
+
+    @Column(name = "profile_img") @NotBlank
+    val profileImg: String = profileImg
 }
