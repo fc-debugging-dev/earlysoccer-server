f8b4072ba09fd07245d4615399f354c81dc583ec
KangJuSeong <wntjd3686@gmail.com>
2023-05-14 15:00:30 +0900

    chore. TeamMemberRepository 분리

--- src/main/kotlin/com/example/earlysoccerserver/domain/team/TeamMemberRepository.kt
+++ src/main/kotlin/com/example/earlysoccerserver/domain/team/TeamMemberRepository.kt
@@ -2,5 +2,5 @@ package com.example.earlysoccerserver.domain.team
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TeamRepository: JpaRepository<Team, Long> {
+interface TeamMemberRepository: JpaRepository<TeamMember, Long> {
 }
