b9210888290922fa47fb266578a02cf8edb14b50
KangJuSeong <wntjd3686@gmail.com>
2023-05-22 13:09:11 +0900

    test: 팀의 스케줄을 가져오는 테스트 코드 위치 변경

--- src/test/kotlin/com/fcdebug/earlysoccerserver/schedule/ScheduleTest.kt
+++ src/test/kotlin/com/fcdebug/earlysoccerserver/schedule/ScheduleTest.kt
@@ -5,6 +5,7 @@ import com.fcdebug.earlysoccerserver.domain.schedule.ScheduleRepository
 import com.fcdebug.earlysoccerserver.domain.team.Team
 import com.fcdebug.earlysoccerserver.domain.team.TeamRepository
 import io.github.serpro69.kfaker.faker
+import jakarta.transaction.Transactional
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
@@ -19,8 +20,34 @@ class ScheduleTest @Autowired constructor (
     private val scheduleRepository: ScheduleRepository,
 ) {
     val faker = faker {  }
+
     @Test
-    fun `스케줄을 생성한다`() {
+    @Transactional
+    fun `팀의 전체 스케줄을 가져온다`() {
+        //given
+        val team: Team = teamRepository.save(
+            Team.create(faker.team.name())
+        )
+
+        //when
+        val schedules = listOf(
+            Schedule.create(
+                team = team,
+                date = LocalDateTime.now(),
+                place = faker.address.fullAddress(),
+                opponent = faker.team.name(),))
+        scheduleRepository.saveAll(schedules)
+        val saveSchedules: List<Schedule> = scheduleRepository.findByTeamId(team.toDto().id!!)
+
+        //then
+        assertThat(saveSchedules[0].team.name).isEqualTo(schedules[0].team.name)
+        assertThat(saveSchedules[0].date).isEqualTo(schedules[0].date)
+        assertThat(saveSchedules[0].place).isEqualTo(schedules[0].place)
+        assertThat(saveSchedules[0].opponent).isEqualTo(schedules[0].opponent)
+    }
+
+    @Test
+    fun `팀의 스케줄을 생성한다`() {
         //given
         val team: Team = teamRepository.save(
             Team.create(faker.team.name())
--- src/test/kotlin/com/fcdebug/earlysoccerserver/team/TeamTest.kt
+++ src/test/kotlin/com/fcdebug/earlysoccerserver/team/TeamTest.kt
@@ -11,39 +11,16 @@ import jakarta.transaction.Transactional
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import java.time.LocalDateTime
 
 @ActiveProfiles("test")
-@SpringBootTest
+@DataJpaTest
 class TeamTest @Autowired constructor (
     private val teamRepository: TeamRepository,
-    private val scheduleRepository: ScheduleRepository,
 ) {
     val faker = faker {  }
-    @Test
-    @Transactional
-    fun `팀의 전체 스케줄을 가져온다`() {
-        //given
-        val team: Team = teamRepository.save(
-            Team.create(faker.team.name())
-        )
 
-        //when
-        val schedules = listOf(
-            Schedule.create(
-                team = team,
-                date = LocalDateTime.now(),
-                place = faker.address.fullAddress(),
-                opponent = faker.team.name(),))
-        scheduleRepository.saveAll(schedules)
-        val saveSchedules: List<Schedule> = scheduleRepository.findByTeamId(team.toDto().id!!)
-
-        //then
-        assertThat(saveSchedules[0].team.name).isEqualTo(schedules[0].team.name)
-        assertThat(saveSchedules[0].date).isEqualTo(schedules[0].date)
-        assertThat(saveSchedules[0].place).isEqualTo(schedules[0].place)
-        assertThat(saveSchedules[0].opponent).isEqualTo(schedules[0].opponent)
-    }
 }
